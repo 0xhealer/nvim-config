993a7aa0878b94105d0df1d7e98f96cfc8b77254
0xhealer <76742567+0xhealer@users.noreply.github.com>
2026-09-04 20:26:07 +0530

    Add Windows Installation Script

---
+++ install.ps1
@@ -0,0 +1,52 @@
+Set-ExecutionPolicy -ExecutionPolicy Bypass -Scope CurrentUser -Force
+Set-StrictMode -Version Latest
+$nvim_config="$env:LOCALAPPDATA\nvim"
+$nvim_bkp_config="$env:LOCALAPPDATA\nvim-bkp"
+
+
+
+$Packages=@(
+"Microsoft.PowerShell"
+"Microsoft.WindowsTerminal"
+"Git.Git"
+"BurntSushi.ripgrep.MSVC"
+"sharkdp.fd"
+"GnuWin32.UnZip"
+"OpenJS.NodeJS.LTS"
+"Python.Python.3.12"
+"Rustlang.Rust.MSVC"
+"Neovim.Neovim"
+)
+
+# Ensures the winget sources are fresh
+winget source update --disable-interactivity
+
+foreach ($Package in $Packages) {
+        winget install `
+        --id $Package `
+        -e `
+        --source winget `
+        --accept-source-agreements `
+        --accept-package-agreements `
+        --silent
+}
+
+# Check the nvim version
+nvim --version
+
+# Installing scoop to install gcc
+Invoke-RestMethod -Uri https://get.scoop.sh | Invoke-Expression
+scoop bucket add extras
+
+scoop install gcc cmake make
+
+# Copy the nvim folder to AppData/Local/
+
+if (Test-Path -Path $nvim_bkp_config){
+        Remove-Item $nvim_bkp_config
+}
+
+if (Test-Path -Path $nvim_config) {
+    mv $nvim_config $nvim_bkp_config
+}
+cp -Recurse $PSScriptRoot\nvim $nvim_config
